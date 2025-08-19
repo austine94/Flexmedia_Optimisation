6e7e3a927b53e8ead8781903f67571bfb605bb95
Edward Austin <63097423+austine94@users.noreply.github.com>
2025-08-19 15:03:30 +0100

    Add files via upload

--- Code/flexmedia_delivery_instance.py
+++ Code/flexmedia_delivery_instance.py
@@ -220,14 +220,14 @@ class flexmedia_delivery_instance:
         else:
             print("Warning: No optimal solution found.")
             
-    def print_solution(self):
+    def print_exact_solution(self):
         '''
-        This method prints out the solution should it exist, showing which
+        This method prints out the exact solution should it exist, showing which
         consumers go from each DC. Will be of limited to no use for large
         instances!
         '''
         if self.optimisation_model is None:
-            print("Optimisation istance has not been solved yet")
+            print("Optimisation istance has not been solved exactly yet")
             return
         
         for i in self.open_centres:
@@ -235,9 +235,9 @@ class flexmedia_delivery_instance:
             for j in self.assignments[i]:
                 print(f"  - Viewer {j} assigned")
                 
-    def plot_solution(self):
+    def plot_exact_solution(self):
         '''
-        This method plots the solution, with each DC a different coloured
+        This method plots the exact solution, with each DC a different coloured
         square, and each consumer a dot coloured according to the DC it is 
         assigned to.
         
@@ -247,7 +247,7 @@ class flexmedia_delivery_instance:
         '''
         
         if self.optimisation_model is None:
-            print("Optimisation istance has not been solved yet")
+            print("Optimisation istance has not been solved exactly yet")
             return
         
         m = len(self.datacentre_costs)    # number of data‐centres
@@ -277,13 +277,129 @@ class flexmedia_delivery_instance:
         plt.ylabel('y')
         plt.tight_layout()
         plt.show()
-                
-                
+        
+    def _solve_facility_subproblem(self, i, lambda_vec, D, c, d, alpha, B):
+        """
+        This function is designed to be used as part of the Lagrangian solver, 
+        rather than called directly. 
+        
+        It solves (using Gurobi) the inner minimisation problem:
+            phi_i(lambda) = min_{x_i., y_i} D_i y_i +
+                            sum_j (c_ij + alpha d_ij - lambda_j) x_ij
+             s.t. x_ij <= y_i, sum_j x_ij <= B_i y_i, x_ij, y_i in {0,1}
+        
+        The function returns the min objective value, and then x_i and y_ values.
+        x_i will be a length n_consumers array of 0/1s, and y_i is a binary 0/1.
+        
+        For clarity the inputs are:
+            - i: the index of the DC
+            - lambda_vec: the lambda values, one per consumer
+            - D: the cost of opening a datacentre
+            - c: the consumer costs for this datacentre.
+            - d: the distances of the consumers to this datacentre.
+            - alpha: the QoE weight.
+            - B: the DC capacities
+        
+        """
+        n = c.shape[1]
+        model = gp.Model()
+        model.setParam('OutputFlag', 0)
+    
+        y = model.addVar(vtype=GRB.BINARY, name=f"y_{i}")
+        x = model.addVars(n, vtype=GRB.BINARY, name=f"x_{i}_")
+    
+        #The solver for the inner min centres around the reduced costs, which are:
+        reduced_costs = c[i, :] + alpha * d[i, :] - lambda_vec  
+        model.setObjective(D[i] * y + gp.quicksum(reduced_costs[j] * x[j] for j in range(n)), GRB.MINIMIZE)
+    
+        model.addConstrs(x[j] <= y for j in range(n))
+        model.addConstr(gp.quicksum(x[j] for j in range(n)) <= B[i] * y)
+    
+        model.optimize()
+    
+        #Extract the current solution (as int) decision variables
+        x_i = (np.array([x[j].X for j in range(n)]) > 0.5).astype(int)
+        y_i = int(y.X > 0.5)
+        return model.objVal, x_i, y_i
+    
         
         
+    def lagrangian_solve(self, alpha, T=200, theta=1.0, UB=np.inf):
+        '''
+        This function implements a solver for the flexmedia instance
+        by formulating it as a Lagrangian relaxation, solving its dual,
+        and then repairing the solution so that it is feasible for the 
+        unrelaxed primal.
         
+        The approach used to update lambda at each step is based on the 
+        subgradient. We compute the step size using the subgradient and then
+        use the Polyak method to update the lambda values based on this step
+        size.
+        '''
         
+    
+        D = self.datacentre_costs           #costs to open
+        B = self.datacentre_capacities #centre capacities
+        c = self.consumer_costs             #consumer delivery costs
+        d = self.consumer_latencies #latencies/distances
+    
+        m, n = c.shape
+        lambda_vec = np.zeros(n)  #Start with zeroes for lambda 
+        #note that the lambda values are free so can be any real
+        best_L = -np.inf
+        best_iter = None
+    
+        for t in range(1, T + 1):
+            phi_vals = np.zeros(m)
+            x_stack = np.zeros((m, n), dtype=int)
+            y_open = np.zeros(m, dtype=int)
+    
+            #Solve each facility subproblems
+            for i in range(m):
+                phi_i, x_i, y_i = self._solve_facility_subproblem(i, lambda_vec, D,
+                                                             c, d, alpha, B)
+                phi_vals[i] = phi_i
+                x_stack[i, :] = x_i
+                y_open[i] = y_i
+    
+            #Compute the value of the Lagrangian objective
+            L_val = lambda_vec.sum() + phi_vals.sum()
+            if L_val > best_L:
+                best_L = L_val
+                best_iter = t
+    
+            #Subgradient g_j = 1 - sum_i x_ij
+            #Compute the subgradient and use it to compute the step size
+            g = 1.0 - x_stack.sum(axis=0)
+            gnorm2 = float(np.dot(g, g))
+    
+            #If UB is known and g != 0 use Polyak update; else diminishing
+            if np.isfinite(UB) and gnorm2 > 1e-12 and (UB - L_val) > 0:
+                step = theta * (UB - L_val) / gnorm2
+            else:
+                step = theta / np.sqrt(t)
+    
+            #update lambda vec using the subgradient based step size.
+            lambda_vec = lambda_vec + step * g
         
+            # Early exit if subgradient is zero (optimal for given UB)
+            if gnorm2 < 1e-12:
+                break
+            
+        #####
+        #Repair Lagrangian Solution To Obtain Feasible 
+        #####
+    
+    
+        #####
+        #Update Class
+        #####
+        
+        self.lagrangian_bound = best_L
+        self.lambda_vec = lambda_vec
+        self.optimisation_model = "Lagrangian"
+    
+            
         
         
         
